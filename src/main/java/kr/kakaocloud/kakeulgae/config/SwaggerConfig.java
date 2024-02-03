bf134a37484f72cc47cdd5ef77d8f68957dad289
Chae <oumuamua121@gmail.com>
2024-02-03 10:55:46 +0900

    Swagger title 변경

--- src/main/java/kr/kakaocloud/kakeulgae/config/SwaggerConfig.java
+++ src/main/java/kr/kakaocloud/kakeulgae/config/SwaggerConfig.java
@@ -5,8 +5,8 @@ import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.context.annotation.Configuration;
 
 @OpenAPIDefinition(
-        info = @Info(title = "User-Service API 명세서",
-        description = "사용자 어플 서비스 API 명세서",
+        info = @Info(title = "API 명세서",
+        description = "개인 맞춤 취업 공고 알림 서비스 API 명세서",
         version = "v1"))
 @Configuration
 public class SwaggerConfig {
