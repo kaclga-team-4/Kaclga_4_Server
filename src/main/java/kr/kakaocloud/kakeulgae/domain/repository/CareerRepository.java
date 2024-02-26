d998f97cd64c2ed9f97b754ea5d0cb61f66c369e
koosco <rnxogud123@naver.com>
2024-02-26 11:52:43 +0900

    test 파일 삭제

--- src/main/java/kr/kakaocloud/kakeulgae/domain/repository/CareerRepository.java
+++
@@ -1,8 +0,0 @@
-package kr.kakaocloud.kakeulgae.domain.repository;
-
-import kr.kakaocloud.kakeulgae.domain.entity.Career;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CareerRepository extends JpaRepository<Career, Long> {
-
-}
