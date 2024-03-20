c29b135a0a5546cd4b5f51d60bbb625918e16dce
koosco <rnxogud123@naver.com>
2024-03-20 09:47:45 +0900

    http status 추가

--- src/main/java/kr/kakaocloud/kakeulgae/controller/JobPostingController.java
+++ src/main/java/kr/kakaocloud/kakeulgae/controller/JobPostingController.java
@@ -6,7 +6,9 @@ import kr.kakaocloud.kakeulgae.service.dto.jobposting.JobPostingListDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Slice;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -15,6 +17,7 @@ public class JobPostingController {
 
     private final JobPostingService jobPostingService;
 
+    @ResponseStatus(HttpStatus.OK)
     @GetMapping("/jobs")
     public Slice<JobPostingListDto> jobs(@LoginUserId Long memberId, Pageable pageable) {
         return jobPostingService.findJobPostings(memberId, pageable);
