78613f9a203e6621e7e6b20dbe1b9b2fe03fcafd
koosco <rnxogud123@naver.com>
2024-03-22 17:49:49 +0900

    관심 카테고리 조회 기능 추가 -> 화면에 뿌리기
    * region1st, region2nd getter 추가, 생성자 제거

---
+++ src/main/java/kr/kakaocloud/kakeulgae/controller/CategoryController.java
@@ -0,0 +1,45 @@
+package kr.kakaocloud.kakeulgae.controller;
+
+import java.util.List;
+import kr.kakaocloud.kakeulgae.domain.entity.Career;
+import kr.kakaocloud.kakeulgae.domain.entity.Education;
+import kr.kakaocloud.kakeulgae.domain.entity.JobDetail;
+import kr.kakaocloud.kakeulgae.domain.entity.Region1st;
+import kr.kakaocloud.kakeulgae.domain.entity.WorkType;
+import kr.kakaocloud.kakeulgae.repository.CareerRepository;
+import kr.kakaocloud.kakeulgae.repository.EducationRepository;
+import kr.kakaocloud.kakeulgae.repository.JobDetailRepository;
+import kr.kakaocloud.kakeulgae.repository.Region1stRepository;
+import kr.kakaocloud.kakeulgae.repository.WorkTypeRepository;
+import kr.kakaocloud.kakeulgae.service.dto.category.CategoryResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class CategoryController {
+
+    private final JobDetailRepository jobDetailRepository;
+    private final Region1stRepository region1stRepository;
+    private final CareerRepository careerRepository;
+    private final WorkTypeRepository workTypeRepository;
+    private final EducationRepository educationRepository;
+
+    @GetMapping("/categories")
+    public CategoryResponse getCategories() {
+        List<JobDetail> jobDetailList = jobDetailRepository.findAll();
+        List<Career> careerList = careerRepository.findAll();
+        List<Region1st> region1stList = region1stRepository.findAll();
+        List<WorkType> workTypeList = workTypeRepository.findAll();
+        List<Education> educationList = educationRepository.findAll();
+
+        return CategoryResponse.builder()
+            .jobDetails(jobDetailList)
+            .careers(careerList)
+            .region(region1stList)
+            .workTypes(workTypeList)
+            .educations(educationList)
+            .build();
+    }
+}
