ac9dde0ca0b97691cef1452ff2fe73f37f777537
koosco <rnxogud123@naver.com>
2024-03-25 00:25:07 +0900

    MemberInterestResponse 반환 프로퍼티 수정
    * jobs -> jobDetails
    * region1st 제거

--- src/main/java/kr/kakaocloud/kakeulgae/service/dto/member/interest/MemberInterestResponse.java
+++ src/main/java/kr/kakaocloud/kakeulgae/service/dto/member/interest/MemberInterestResponse.java
@@ -1,13 +1,14 @@
 package kr.kakaocloud.kakeulgae.service.dto.member.interest;
 
 import java.util.List;
+import lombok.Builder;
 import lombok.Data;
 
 @Data
+@Builder
 public class MemberInterestResponse {
 
-    private List<String> jobs;
-    private List<String> region1sts;
+    private List<String> jobDetails;
     private List<String> region2nds;
     private List<String> careers;
     private List<String> educations;
