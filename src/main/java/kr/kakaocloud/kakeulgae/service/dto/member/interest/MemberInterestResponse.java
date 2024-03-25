c16fdcf0b94b26fac2d01248a1a0121acce7fa9c
koosco <rnxogud123@naver.com>
2024-03-25 18:20:56 +0900

    memberInterest를 반환할 때 Id를 같이 반환하도록 수정

--- src/main/java/kr/kakaocloud/kakeulgae/service/dto/member/interest/MemberInterestResponse.java
+++ src/main/java/kr/kakaocloud/kakeulgae/service/dto/member/interest/MemberInterestResponse.java
@@ -1,16 +1,39 @@
 package kr.kakaocloud.kakeulgae.service.dto.member.interest;
 
 import java.util.List;
+import kr.kakaocloud.kakeulgae.domain.entity.Career;
+import kr.kakaocloud.kakeulgae.domain.entity.Education;
+import kr.kakaocloud.kakeulgae.domain.entity.JobDetail;
+import kr.kakaocloud.kakeulgae.domain.entity.Region2nd;
+import kr.kakaocloud.kakeulgae.domain.entity.WorkType;
 import lombok.Builder;
 import lombok.Data;
 
 @Data
-@Builder
 public class MemberInterestResponse {
 
-    private List<String> jobDetails;
-    private List<String> region2nds;
-    private List<String> careers;
-    private List<String> educations;
-    private List<String> workTypes;
+    private List<JobDetailInterestDto> jobDetails;
+    private List<Region2ndInterestDto> region2nds;
+    private List<CareerInterestDto> careers;
+    private List<EducationInterestDto> educations;
+    private List<WorkTypeInterestDto> workTypes;
+
+    @Builder
+    public MemberInterestResponse(List<JobDetail> jobDetails, List<Region2nd> region2nds,
+        List<Career> careers, List<Education> educations, List<WorkType> workTypes) {
+        this.jobDetails = jobDetails.stream()
+            .map(JobDetailInterestDto::new).toList();
+
+        this.region2nds = region2nds.stream()
+            .map(Region2ndInterestDto::new).toList();
+
+        this.careers = careers.stream()
+            .map(CareerInterestDto::new).toList();
+
+        this.educations = educations.stream()
+            .map(EducationInterestDto::new).toList();
+
+        this.workTypes = workTypes.stream()
+            .map(WorkTypeInterestDto::new).toList();
+    }
 }
