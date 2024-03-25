77e980a59190336a243f06b339da4a0538501c0e
koosco <rnxogud123@naver.com>
2024-03-25 09:51:51 +0900

    feat : findByMember 메서드 추가

--- src/main/java/kr/kakaocloud/kakeulgae/repository/PreferenceJobRepository.java
+++ src/main/java/kr/kakaocloud/kakeulgae/repository/PreferenceJobRepository.java
@@ -1,8 +1,12 @@
 package kr.kakaocloud.kakeulgae.repository;
 
+import java.util.List;
+import kr.kakaocloud.kakeulgae.domain.entity.JobDetail;
+import kr.kakaocloud.kakeulgae.domain.entity.member.Member;
 import kr.kakaocloud.kakeulgae.domain.entity.member.PreferenceJob;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface PreferenceJobRepository extends JpaRepository<PreferenceJob, Long> {
 
+    List<PreferenceJob> findByMember(Member member);
 }
