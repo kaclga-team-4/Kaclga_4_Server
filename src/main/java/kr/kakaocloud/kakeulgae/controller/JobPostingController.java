1a694861eeaf0f775fac5d76b517b7d04e5bbde7
koosco <rnxogud123@naver.com>
2024-03-25 17:12:12 +0900

    JobPostingController/service : 검색 메서드 추가

--- src/main/java/kr/kakaocloud/kakeulgae/controller/JobPostingController.java
+++ src/main/java/kr/kakaocloud/kakeulgae/controller/JobPostingController.java
@@ -3,11 +3,14 @@ package kr.kakaocloud.kakeulgae.controller;
 import kr.kakaocloud.kakeulgae.security.LoginUserId;
 import kr.kakaocloud.kakeulgae.service.JobPostingService;
 import kr.kakaocloud.kakeulgae.service.dto.jobposting.JobPostingListDto;
+import kr.kakaocloud.kakeulgae.service.dto.jobposting.JobPostingSearchCondition;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Slice;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,7 +22,8 @@ public class JobPostingController {
 
     @ResponseStatus(HttpStatus.OK)
     @GetMapping("/jobs/details")
-    public Slice<JobPostingListDto> jobsAndjobDetails(@LoginUserId Long memberId, Pageable pageable) {
+    public Slice<JobPostingListDto> jobsAndjobDetails(@LoginUserId Long memberId,
+        Pageable pageable) {
         return jobPostingService.findJobPostingsByDetails(memberId, pageable);
     }
 
@@ -29,5 +33,10 @@ public class JobPostingController {
         return jobPostingService.findPostings(pageable);
     }
 
-
+    @ResponseStatus(HttpStatus.OK)
+    @PostMapping("/jobs/search")
+    public Slice<JobPostingListDto> searchJobsAndJobDetails(@LoginUserId Long memberId,
+        @ModelAttribute JobPostingSearchCondition condition, Pageable pageable) {
+        return jobPostingService.searchJobPosting(memberId, condition, pageable);
+    }
 }
--- src/main/java/kr/kakaocloud/kakeulgae/service/JobPostingService.java
+++ src/main/java/kr/kakaocloud/kakeulgae/service/JobPostingService.java
@@ -1,10 +1,10 @@
 package kr.kakaocloud.kakeulgae.service;
 
-import kr.kakaocloud.kakeulgae.repository.JobDetailRepository;
+import kr.kakaocloud.kakeulgae.domain.entity.JobPosting;
 import kr.kakaocloud.kakeulgae.repository.JobPostingRepository;
 import kr.kakaocloud.kakeulgae.service.dto.jobposting.JobPostingListDto;
+import kr.kakaocloud.kakeulgae.service.dto.jobposting.JobPostingSearchCondition;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Slice;
 import org.springframework.stereotype.Service;
@@ -23,5 +23,11 @@ public class JobPostingService {
     public Slice<JobPostingListDto> findJobPostingsByDetails(Long memberId, Pageable pageable) {
         return jobPostingRepository.findAllByMemberId(memberId, pageable)
             .map(JobPostingListDto::new);
+    }
+
+    public Slice<JobPostingListDto> searchJobPosting(Long memberId,
+        JobPostingSearchCondition condition, Pageable pageable) {
+        Slice<JobPosting> results = jobPostingRepository.search(memberId, condition, pageable);
+        return results.map(JobPostingListDto::new);
     }
 }
