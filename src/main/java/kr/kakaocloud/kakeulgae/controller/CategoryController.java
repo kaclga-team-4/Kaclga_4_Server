8847a1c431d853d66a48eda8a60bc8540b8b2ca6
koosco <rnxogud123@naver.com>
2024-03-26 15:43:15 +0900

    controller : modelattribute -> requestbody

--- src/main/java/kr/kakaocloud/kakeulgae/controller/CategoryController.java
+++ src/main/java/kr/kakaocloud/kakeulgae/controller/CategoryController.java
@@ -18,8 +18,8 @@ import kr.kakaocloud.kakeulgae.service.dto.member.interest.MemberInterestRequest
 import kr.kakaocloud.kakeulgae.service.dto.member.interest.MemberInterestResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -52,7 +52,7 @@ public class CategoryController {
 
     @PostMapping("/interest/create")
     public MemberInterestResponse createInterest(@LoginUserId Long id,
-        @ModelAttribute MemberInterestRequest request) {
+        @RequestBody MemberInterestRequest request) {
         return memberService.createUserInterest(id, request);
     }
 }
