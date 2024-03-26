08c634c2883e3e879480270f38c383ccad10852d
koosco <rnxogud123@naver.com>
2024-03-26 15:11:32 +0900

    @OneToMany - jobDetail 연관관계 추가

--- src/main/java/kr/kakaocloud/kakeulgae/domain/entity/JobCategory.java
+++ src/main/java/kr/kakaocloud/kakeulgae/domain/entity/JobCategory.java
@@ -1,14 +1,16 @@
 package kr.kakaocloud.kakeulgae.domain.entity;
 
-import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Index;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import jakarta.persistence.UniqueConstraint;
 import jakarta.validation.constraints.NotEmpty;
+import java.util.ArrayList;
+import java.util.List;
 import lombok.Getter;
 
 @Getter
@@ -18,21 +20,21 @@ import lombok.Getter;
     uniqueConstraints = @UniqueConstraint(name = "job_category_uk", columnNames = "type"),
     indexes = @Index(name = "job_category_idx_type", columnList = "type")
 )
-public class JobCategory {
+public class JobCategory implements Comparable {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-
     @NotEmpty
     private String type;
 
-    public JobCategory(String content) {
-        this.type = content;
-    }
+    @OneToMany(mappedBy = "jobCategory")
+    private List<JobDetail> jobDetails = new ArrayList<>();
 
-    public JobCategory() {
-
+    @Override
+    public int compareTo(Object o) {
+        JobCategory jobCategory = (JobCategory) o;
+        return this.getType().compareTo(jobCategory.getType());
     }
 }
