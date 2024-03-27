0574fae07d7e3f11c33366626a4b9f20fc0238f1
koosco <rnxogud123@naver.com>
2024-03-27 11:48:08 +0900

    merge 에러 수정

--- src/main/java/kr/kakaocloud/kakeulgae/controller/CategoryController.java
+++ src/main/java/kr/kakaocloud/kakeulgae/controller/CategoryController.java
@@ -1,26 +1,14 @@
 package kr.kakaocloud.kakeulgae.controller;
 
-import java.util.List;
-import kr.kakaocloud.kakeulgae.domain.entity.Career;
-import kr.kakaocloud.kakeulgae.domain.entity.Education;
-import kr.kakaocloud.kakeulgae.domain.entity.JobCategory;
-import kr.kakaocloud.kakeulgae.domain.entity.JobDetail;
-import kr.kakaocloud.kakeulgae.domain.entity.Region1st;
-import kr.kakaocloud.kakeulgae.domain.entity.WorkType;
-import kr.kakaocloud.kakeulgae.repository.CareerRepository;
-import kr.kakaocloud.kakeulgae.repository.EducationRepository;
-import kr.kakaocloud.kakeulgae.repository.JobCategoryRepository;
-import kr.kakaocloud.kakeulgae.repository.JobDetailRepository;
-import kr.kakaocloud.kakeulgae.repository.Region1stRepository;
-import kr.kakaocloud.kakeulgae.repository.WorkTypeRepository;
 import kr.kakaocloud.kakeulgae.security.LoginUserId;
-import kr.kakaocloud.kakeulgae.service.MemberService;
+import kr.kakaocloud.kakeulgae.service.CategoryService;
 import kr.kakaocloud.kakeulgae.service.dto.category.CategoryResponse;
 import kr.kakaocloud.kakeulgae.service.dto.member.interest.MemberInterestRequest;
 import kr.kakaocloud.kakeulgae.service.dto.member.interest.MemberInterestResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -28,34 +16,22 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 public class CategoryController {
 
-    private final JobCategoryRepository jobCategoryRepository;
-    private final MemberService memberService;
-    private final JobDetailRepository jobDetailRepository;
-    private final Region1stRepository region1stRepository;
-    private final CareerRepository careerRepository;
-    private final WorkTypeRepository workTypeRepository;
-    private final EducationRepository educationRepository;
+    private final CategoryService categoryService;
 
     @GetMapping("/categories")
     public CategoryResponse getCategories() {
-        List<JobCategory> categoryList = jobCategoryRepository.findAll();
-        List<Career> careerList = careerRepository.findAll();
-        List<Region1st> region1stList = region1stRepository.findAll();
-        List<WorkType> workTypeList = workTypeRepository.findAll();
-        List<Education> educationList = educationRepository.findAll();
-
-        return CategoryResponse.builder()
-            .jobCategories(categoryList)
-            .careers(careerList)
-            .region(region1stList)
-            .workTypes(workTypeList)
-            .educations(educationList)
-            .build();
+        return categoryService.findAllCategories();
     }
 
     @PostMapping("/interest/create")
     public MemberInterestResponse createInterest(@LoginUserId Long id,
         @RequestBody MemberInterestRequest request) {
-        return memberService.createUserInterest(id, request);
+        return categoryService.createUserInterest(id, request);
+    }
+
+    @PutMapping("/interest/update")
+    public MemberInterestResponse updateInterest(@LoginUserId Long id,
+        @RequestBody MemberInterestRequest request) {
+        return categoryService.updateUserInterest(id, request);
     }
 }
--- src/main/java/kr/kakaocloud/kakeulgae/service/CareerService.java
+++ src/main/java/kr/kakaocloud/kakeulgae/service/CareerService.java
@@ -17,6 +17,10 @@ public class CareerService {
     private final CareerRepository careerRepository;
     private final CareerMemberRepository careerMemberRepository;
 
+    public List<Career> findAllCareer() {
+        return careerRepository.findAll();
+    }
+
     public List<Career> saveUserCareer(Member member, List<Long> careerIds) {
         List<Career> careers = careerRepository.findByIdIn(careerIds);
         ArrayList<CareerMemberRelation> careerMemberRelations = new ArrayList<>();
@@ -24,6 +28,22 @@ public class CareerService {
             careerMemberRelations.add(CareerMemberRelation.createRelation(member, career));
         }
         careerMemberRepository.saveAll(careerMemberRelations);
+
+        return careers;
+    }
+
+    public List<Career> updateUserCareer(Member member, List<Long> careerIds) {
+        List<Career> careers = careerRepository.findByIdIn(careerIds);
+        List<CareerMemberRelation> careerMemberRelations = careerMemberRepository.findByMember(
+            member);
+        careerMemberRepository.deleteAll(careerMemberRelations);
+
+        List<CareerMemberRelation> newCareerMemberRelations = new ArrayList<>();
+
+        for (Career career : careers) {
+            newCareerMemberRelations.add(CareerMemberRelation.createRelation(member, career));
+        }
+        careerMemberRepository.saveAll(newCareerMemberRelations);
 
         return careers;
     }
