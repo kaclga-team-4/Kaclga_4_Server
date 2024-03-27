ce45113b4f5f76254aefd15e510ff11e71dc7ca1
koosco <rnxogud123@naver.com>
2024-03-27 11:56:33 +0900

    library import

--- src/main/java/kr/kakaocloud/kakeulgae/controller/CategoryController.java
+++ src/main/java/kr/kakaocloud/kakeulgae/controller/CategoryController.java
@@ -1,15 +1,15 @@
 package kr.kakaocloud.kakeulgae.controller;
 
-import java.util.List;
-import kr.kakaocloud.kakeulgae.domain.entity.Career;
-import kr.kakaocloud.kakeulgae.domain.entity.Education;
-import kr.kakaocloud.kakeulgae.domain.entity.JobDetail;
-import kr.kakaocloud.kakeulgae.domain.entity.Region1st;
-import kr.kakaocloud.kakeulgae.domain.entity.WorkType;
+import kr.kakaocloud.kakeulgae.security.LoginUserId;
 import kr.kakaocloud.kakeulgae.service.CategoryService;
 import kr.kakaocloud.kakeulgae.service.dto.category.CategoryResponse;
+import kr.kakaocloud.kakeulgae.service.dto.member.interest.MemberInterestRequest;
+import kr.kakaocloud.kakeulgae.service.dto.member.interest.MemberInterestResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
