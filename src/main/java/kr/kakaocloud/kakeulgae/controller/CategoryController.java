817cfb974c401a56baaebbe5875c52b503dfb85b
koosco <rnxogud123@naver.com>
2024-03-27 11:48:08 +0900

    사용자 관심 카테고리 update API 추가

--- src/main/java/kr/kakaocloud/kakeulgae/controller/CategoryController.java
+++ src/main/java/kr/kakaocloud/kakeulgae/controller/CategoryController.java
@@ -20,6 +20,8 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -35,7 +37,13 @@ public class CategoryController {
 
     @PostMapping("/interest/create")
     public MemberInterestResponse createInterest(@LoginUserId Long id,
-        @ModelAttribute MemberInterestRequest request) {
+        @RequestBody MemberInterestRequest request) {
         return categoryService.createUserInterest(id, request);
+    }
+
+    @PutMapping("/interest/update")
+    public MemberInterestResponse updateInterest(@LoginUserId Long id,
+        @RequestBody MemberInterestRequest request) {
+        return categoryService.updateUserInterest(id, request);
     }
 }
