bd1477fdea7460e26089e06b48afc7d4d26a62d7
Chae <oumuamua121@gmail.com>
2024-03-31 23:00:40 +0900

    에러 수정

--- src/main/java/kr/kakaocloud/kakeulgae/service/dto/category/CategoryResponse.java
+++ src/main/java/kr/kakaocloud/kakeulgae/service/dto/category/CategoryResponse.java
@@ -3,7 +3,6 @@ package kr.kakaocloud.kakeulgae.service.dto.category;
 import java.util.List;
 import kr.kakaocloud.kakeulgae.domain.entity.Career;
 import kr.kakaocloud.kakeulgae.domain.entity.Education;
-import kr.kakaocloud.kakeulgae.domain.entity.JobCategory;
 import kr.kakaocloud.kakeulgae.domain.entity.JobDetail;
 import kr.kakaocloud.kakeulgae.domain.entity.Region1st;
 import kr.kakaocloud.kakeulgae.domain.entity.WorkType;
@@ -13,17 +12,17 @@ import lombok.Data;
 @Data
 public class CategoryResponse {
 
-    private List<CategoryJobCategory> jobCategories;
+    private List<CategoryJobDetailDto> jobDetails;
     private List<CategoryRegionResponse> region;
     private List<CategoryCareerDto> career;
     private List<CategoryEducationDto> education;
     private List<CategoryWorkTypeDto> work_type;
 
     @Builder
-    public CategoryResponse(List<JobCategory> jobCategories, List<JobDetail> jobDetails,
+    public CategoryResponse(List<JobDetail> jobDetails,
         List<Region1st> region,
         List<Career> careers, List<Education> educations, List<WorkType> workTypes) {
-        this.jobCategories = jobCategories.stream().map(CategoryJobCategory::new).toList();
+        this.jobDetails = jobDetails.stream().map(CategoryJobDetailDto::new).toList();
         this.region = region.stream()
             .map(CategoryRegionResponse::new)
             .toList();
